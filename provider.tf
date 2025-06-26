c874f983965f504bbd4ea514c08cc325eb4d13ce
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-06-26 18:35:26 +0530

    added provider

--- provider.tf
+++ provider.tf
@@ -8,15 +8,19 @@ terraform {
     #dynamodb_table = "terraform-locks"    
   } 
   } 
+# Terraform Block
+terraform {
+  #required_version = ">= 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  required_version = ">= 1.6.0"   
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 5.31"
     }
   }
-
-  required_version = ">= 1.9.0"
 }
+
+# Provider Block
 provider "aws" {
-  region = "us-east-1"
+  region  = var.aws_region
 }
