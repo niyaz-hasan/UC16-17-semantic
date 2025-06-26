28480342534eddccdccebb431505ab1724a5f2d4
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-06-26 21:05:44 +0530

    remove the backend

--- provider.tf
+++ provider.tf
@@ -1,13 +1,13 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-remote-st"
-    key    = "semantic/terraform.tfstate"
-    region = "us-east-1" 
- 
-    # For State Locking
-    #dynamodb_table = "terraform-locks"    
-  } 
-  } 
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-remote-st"
+#    key    = "semantic/terraform.tfstate"
+#    region = "us-east-1" 
+# 
+#    # For State Locking
+#    #dynamodb_table = "terraform-locks"    
+#  } 
+#  } 
 # Terraform Block
 terraform {
   #required_version = ">= 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
